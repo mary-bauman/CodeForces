42daef0b01b5bb119f6d517502b35f54af8f21d6
Mary <marybauman503@gmail.com>
2024-07-26 19:55:41 -0400

    structure

--- 959/F/F.py
+++ 959/F/F.py
@@ -1,17 +1,22 @@
 numberOfTestCases = int(input())
 for testCase in range(numberOfTestCases):
     print(" ")
-    housesRoads = input().split()
-    numberOfHouses = int(housesRoads[0])
-    numberOfRoads = int(housesRoads[1])
-    arr = []
-    for road in range(numberOfRoads):
-        arr.append(input().split())
-    print(f"arr = roadPoint1, roadPoint2,if c==1 then npc else no npc")
-    print(arr)
+    numberOfHouses,numberOfRoads = input().split()
+    arr = [input().split() for road in range(numberOfRoads)]
 
-    solution = "No"
+    # print(f"arr = roadPoint1, roadPoint2,if c==1 then npc else no npc")
+    # print(arr)
+
+    route = []
 
 
-    print(solution)
+
+
+
+
+    if route:
+        print("Yes")
+        print(len(route))
+        print(route)
+    else: print("No")
 
