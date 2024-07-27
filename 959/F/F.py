4b559315dc0f8cf902729dbe9f713447bd6def88
Mary <marybauman503@gmail.com>
2024-07-26 21:30:09 -0400

    correct answer

--- 959/F/F.py
+++ 959/F/F.py
@@ -4,43 +4,74 @@ numberOfTestCases = int(input())
 for testCase in range(numberOfTestCases):
     print(" ")
     numberOfHouses,numberOfRoads = [int(x) for x in input().split()]
-    
-    connections = defaultdict(list)
-    optionalConnections = defaultdict(list)
-    memo = defaultdict(list)
-    for road in range(numberOfRoads):
-        roadPoint1, roadPoint2, c = input().split()
-        if c == "1":
-            connections[roadPoint1].append(roadPoint2)
-            connections[roadPoint2].append(roadPoint1)
+    npcRoad = []
+    normRoad = []
+
+    for _ in range(numberOfRoads):
+        p1, p2, npc = input().split()
+        if npc == "1":
+            npcRoad.append(f"{p1}{p2}")
         else:
-            optionalConnections[roadPoint1].append(roadPoint2)
-            optionalConnections[roadPoint2].append(roadPoint1)
-    print(connections)
+            normRoad.append(f"{p1}{p2}")
+    
+    print("npc",npcRoad)
+    print("norm",normRoad)
+    print("NPCs",len(npcRoad))
+    
+    queue = [f"0{x}" for x in range(1,numberOfHouses+1)]
+    visited = defaultdict(list)
     route = ""
 
-    queue = [f"{x}" for x in range(1, numberOfHouses+1)]
-    while queue:
-        print(queue)
+    def getRoute():
+        global queue
+        global visited
+        global route
         cur = queue.pop(0)
 
-        if len(cur) == numberOfHouses:
-            print("len(cur) == numberOfHouses")
-            if cur[0] in connections[cur[-1]]:
-                print("break for route = ", cur)
-                route = cur
-                break
-        
-        ext = []
-        for i in connections[cur[-1]]:
-            if f"{cur[-1]}{i}" not in memo[cur] and f"{i}{cur[-1]}" not in memo[cur]:
-                ext.append(cur + str(i))
-                memo[cur + str(i)] = memo[cur] + [f"{cur[-1]}{i}"]
+        if len(set(cur[1:])) == numberOfHouses:
+            print("full house:", cur)
+            if int(cur[0]) == len(npcRoad)-1:
+                for road in npcRoad:
+                    print(f"for npc road {road} in {npcRoad}")
+                    print(road not in visited)
+                    print((cur[-1] in road))
+                    print((cur[1] in road))
+                    if (road not in visited) and (cur[-1] in road) and (cur[1] in road):
+                        return cur[1:] + cur[1]
 
-        print("memo:", memo)
-        print("ext:", ext)
+
+            elif int(cur[0]) == len(npcRoad):
+                for road in normRoad:
+                    print(f"for norm road {road} in {normRoad}")
+                    print(road not in visited)
+                    print((cur[-1] in road))
+                    print((cur[1] in road))
+                    if road not in visited and cur[-1] in road and cur[1] in road:
+                        return cur[1:] + cur[1]
+            
+            
+        ext = []
+        for road in npcRoad:
+            if road not in visited[cur] and cur[-1] in road:
+                new = str(int(cur[0])+1) + cur[1:] + road.replace(cur[-1], "")
+                ext.append(new)
+                visited[new] = visited[cur] + [road]
+            
+        for road in normRoad:
+            if road not in visited[cur] and cur[-1] in road:
+                new = cur + road.replace(cur[-1], "")
+                ext.append(new)
+                visited[new] = visited[cur] + [road]
+        
         queue = queue + ext
-    print(queue)
+
+    while queue:
+        route = getRoute()
+        if route:
+            break
+        
+
+        
     print("route = ", route)
     if route:
         print("Yes")
