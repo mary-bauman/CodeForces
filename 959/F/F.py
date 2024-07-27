dd103ed0fb1c20f573bb4038f3bd9f5e54d08c78
Mary <marybauman503@gmail.com>
2024-07-26 20:19:48 -0400

    before realizing unique roads, not houses

--- 959/F/F.py
+++ 959/F/F.py
@@ -1,19 +1,40 @@
+from collections import defaultdict
+
 numberOfTestCases = int(input())
 for testCase in range(numberOfTestCases):
     print(" ")
-    numberOfHouses,numberOfRoads = input().split()
-    arr = [input().split() for road in range(numberOfRoads)]
+    numberOfHouses,numberOfRoads = [int(x) for x in input().split()]
+    
+    connections = defaultdict(list)
+    for road in range(numberOfRoads):
+        roadPoint1, roadPoint2, c = input().split()
+        if c == "1":
+            connections[roadPoint1].append(roadPoint2)
+            connections[roadPoint2].append(roadPoint1)
+    print(connections)
+    route = ""
 
-    # print(f"arr = roadPoint1, roadPoint2,if c==1 then npc else no npc")
-    # print(arr)
+    queue = [f"{x}" for x in range(1, numberOfHouses+1)]
+    while queue:
+        print(queue)
+        cur = queue.pop(0)
 
-    route = []
+        if len(cur) == numberOfHouses:
+            print("len(cur) == numberOfHouses")
+            if cur[0] in connections[cur[-1]]:
+                print("break for route = ", cur)
+                route = cur
+                break
+        
+        ext = []
+        for i in connections[cur[-1]]:
+            if str(i) not in cur:
+                ext.append(cur + str(i))
 
-
-
-
-
-
+        print("ext:", ext)
+        queue = queue + ext
+    print(queue)
+    print("route = ", route)
     if route:
         print("Yes")
         print(len(route))
