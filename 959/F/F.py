e9d78169b8ee113969405bd8ca01f9e2b5b8b284
Mary <marybauman503@gmail.com>
2024-07-26 21:41:20 -0400

    ready to submit

--- 959/F/F.py
+++ 959/F/F.py
@@ -2,8 +2,7 @@ from collections import defaultdict
 
 numberOfTestCases = int(input())
 for testCase in range(numberOfTestCases):
-    print(" ")
-    numberOfHouses,numberOfRoads = [int(x) for x in input().split()]
+    numberOfHouses, numberOfRoads = [int(x) for x in input().split()]
     npcRoad = []
     normRoad = []
 
@@ -14,41 +13,25 @@ for testCase in range(numberOfTestCases):
         else:
             normRoad.append(f"{p1}{p2}")
     
-    print("npc",npcRoad)
-    print("norm",normRoad)
-    print("NPCs",len(npcRoad))
-    
     queue = [f"0{x}" for x in range(1,numberOfHouses+1)]
     visited = defaultdict(list)
-    route = ""
 
     def getRoute():
         global queue
         global visited
-        global route
+
         cur = queue.pop(0)
 
         if len(set(cur[1:])) == numberOfHouses:
-            print("full house:", cur)
             if int(cur[0]) == len(npcRoad)-1:
                 for road in npcRoad:
-                    print(f"for npc road {road} in {npcRoad}")
-                    print(road not in visited)
-                    print((cur[-1] in road))
-                    print((cur[1] in road))
                     if (road not in visited) and (cur[-1] in road) and (cur[1] in road):
                         return cur[1:] + cur[1]
 
-
             elif int(cur[0]) == len(npcRoad):
                 for road in normRoad:
-                    print(f"for norm road {road} in {normRoad}")
-                    print(road not in visited)
-                    print((cur[-1] in road))
-                    print((cur[1] in road))
                     if road not in visited and cur[-1] in road and cur[1] in road:
-                        return cur[1:] + cur[1]
-            
+                        return cur[1:] + cur[1]     
             
         ext = []
         for road in npcRoad:
@@ -65,17 +48,18 @@ for testCase in range(numberOfTestCases):
         
         queue = queue + ext
 
+    route = ""
     while queue:
         route = getRoute()
         if route:
             break
-        
-
-        
-    print("route = ", route)
+    
     if route:
         print("Yes")
         print(len(route))
-        print(route)
+        ans = route[0]
+        for house in route[1:]:
+            ans += f" {house}"
+        print(ans)
     else: print("No")
 
