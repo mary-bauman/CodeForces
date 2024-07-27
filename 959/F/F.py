837e714178d06a2b035225dc2096c6c7eecd7912
Mary <marybauman503@gmail.com>
2024-07-26 22:07:41 -0400

    we're going to the function

--- 959/F/F.py
+++ 959/F/F.py
@@ -3,18 +3,19 @@ from collections import defaultdict
 numberOfTestCases = int(input())
 for testCase in range(numberOfTestCases):
     numberOfHouses, numberOfRoads = [int(x) for x in input().split()]
-    npcRoad = []
-    normRoad = []
+    npcRoad = defaultdict(int)
+    normRoad = defaultdict(int)
+    numberOfNPCs = sum(npcRoad.values())
 
     for _ in range(numberOfRoads):
         p1, p2, npc = input().split()
         if npc == "1":
-            npcRoad.append(f"{p1}{p2}")
+            npcRoad[f"{p1}{p2}"] += 1
         else:
-            normRoad.append(f"{p1}{p2}")
+            normRoad[f"{p1}{p2}"] += 1
     
     queue = [f"0{x}" for x in range(1,numberOfHouses+1)]
-    visited = defaultdict(list)
+    visited = defaultdict(lambda: defaultdict(int))
 
     def getRoute():
         global queue
@@ -23,28 +24,30 @@ for testCase in range(numberOfTestCases):
         cur = queue.pop(0)
 
         if len(set(cur[1:])) == numberOfHouses:
-            if int(cur[0]) == len(npcRoad)-1:
+            if int(cur[0]) == numberOfNPCs-1:
                 for road in npcRoad:
-                    if (road not in visited) and (cur[-1] in road) and (cur[1] in road):
+                    if (visited[cur][road] - normRoad[road] < npcRoad[road]) and (cur[-1] in road) and (cur[1] in road):
                         return cur[1:] + cur[1]
 
-            elif int(cur[0]) == len(npcRoad):
+            elif int(cur[0]) == numberOfNPCs:
                 for road in normRoad:
-                    if road not in visited and cur[-1] in road and cur[1] in road:
-                        return cur[1:] + cur[1]     
+                    if (visited[cur][road] - npcRoad[road] < normRoad[road]) and cur[-1] in road and cur[1] in road:
+                        return cur[1:] + cur[1]
             
         ext = []
         for road in npcRoad:
-            if road not in visited[cur] and cur[-1] in road:
+            if (visited[cur][road] - normRoad[road] < npcRoad[road]) and cur[-1] in road:
                 new = str(int(cur[0])+1) + cur[1:] + road.replace(cur[-1], "")
                 ext.append(new)
-                visited[new] = visited[cur] + [road]
+                visited[new] = visited[cur]
+                visited[new][road] += 1
             
         for road in normRoad:
-            if road not in visited[cur] and cur[-1] in road:
+            if (visited[cur][road] - npcRoad[road] < normRoad[road]) and cur[-1] in road:
                 new = cur + road.replace(cur[-1], "")
                 ext.append(new)
-                visited[new] = visited[cur] + [road]
+                visited[new] = visited[cur]
+                visited[new][road] += 1
         
         queue = queue + ext
 
@@ -55,11 +58,11 @@ for testCase in range(numberOfTestCases):
             break
     
     if route:
-        print("Yes")
-        print(len(route))
+        print("YES")
+        print(len(route)-1)
         ans = route[0]
         for house in route[1:]:
             ans += f" {house}"
         print(ans)
-    else: print("No")
+    else: print("NO")
 
