214c3072117bae92a8f06eff39e529c40ba5bd9b
Nolan Prochnau <parvus.mortalis@gmail.com>
2024-08-13 16:54:41 -0400

    solution 1

--- 966/C/C.py
+++ 966/C/C.py
@@ -1,1 +1,32 @@
-print("HI")
+t = int(input())
+
+for _ in range(t):
+    n = int(input()) # length of arr
+    a = [int(x) for x in input().split()]
+    m = int(input()) # number of strings
+
+    for _ in range(m):
+        s = input()
+        tbl = {}
+        good = True
+
+        if len(s) != n:
+            print("NO")
+            continue
+
+
+
+        for i in range(n):
+            if s[i] not in tbl:
+                tbl[s[i]] = a[i]
+
+            if a[i] != tbl[s[i]]:
+                good = False
+                break
+
+
+
+        if good:
+            print("YES")
+        else:
+            print("NO")
