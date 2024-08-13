3c1bbf22cd62861a9731eb559be05c44e2413615
Mary <marybauman503@gmail.com>
2024-08-13 16:26:35 -0400

    comp over

--- 966/B/B.py
+++ 966/B/B.py
@@ -1,7 +1,14 @@
+from time import time
+t1 = time()
+
 def seatsGood(numPeople, seats):
+    # seats = list(map(int, seats))
+    #not faster than int(x) for x in seats
     seats = [int(x) for x in seats]
     cur = [seats[0]]
-    seats = seats[1:]  
+    seats = seats[1:]
+    #switching to seats.pop(0) is slower? idk why
+    # cur = [seats.pop(0)]    
     for seat in seats:
         if seat-1 in cur or seat+1 in cur:
             cur.append(seat)
@@ -14,4 +21,9 @@ tests = int(input())
 for test in range(tests):
     if seatsGood(int(input()), input().split()): print("YES")
     else: print("NO")
+t2 = str((time()-t1)*1000)[:4]
+print(f"time = {t2} ms")
+#time = 0.10180473327636719 s
+#time = 0.2760887145996094 s
+#time = 0.1621246337890625 s
 
