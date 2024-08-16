a71a7968e8084c9f7daa27463f2935aaacb1e75d
Mary <marybauman503@gmail.com>
2024-08-15 23:33:06 -0400

    in queue again

--- 169/C/C.py
+++ 169/C/C.py
@@ -1,37 +1,15 @@
 tests = int(input())
 for t in range(tests):
-    # print("\n")
-    arr = input().split()
-    items = int(arr[0])
-    k = int(arr[1])
-    costs = input().split()
-    costs = [int(c) for c in costs]
+    items, k = map(int, input().split())
+    costs = list(map(int, input().split()))
     costs.sort(reverse=True)
-    # print("items = ", items)
-    # print("k = ", k)
-    # print("costs = ", costs)
-    bob = 0
-    alice = 0
-    #score will be alice-bob
-    #both trying to have big scores
-
+    alice, bob = 0, 0
     aliceTurn = True
     for i,cost in enumerate(costs):
-        if aliceTurn:
-            alice+=cost
+        if aliceTurn: alice+=cost
         else:
-            bob+=cost
-            # print("\nelse")
-            # print("bob = ", bob)
-            # print("k = ", k)
-            # print("i = ", i)
-            if k!=0:
-                diff = costs[i-1]-cost
-                change = min(diff,k)
-                bob+=change
-                k-=change
+            change = min((costs[i-1]-cost),k)
+            bob+=(cost + change)
+            k-=change         
         aliceTurn = not aliceTurn
     print(alice-bob)
-    # print("costs = ", costs)
-    # print("bob = ", bob)
-    # print("alice = ", alice)
