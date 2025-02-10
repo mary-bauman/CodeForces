277237dad7ad9acec80c7f10da810e4eda46ad24
Mary <marybauman503@gmail.com>
2025-02-10 13:15:38 -0500

    nolan smart

--- 1566/B/B.py
+++ 1566/B/B.py
@@ -1,6 +1,18 @@
+#nolans code
+
 for _ in range(int(input())):
-    ans = 0
+    s = input()
+    n = int(s, 2)
 
+    cnt = 0
+    cnt += (n & 1) == 0
 
+    for mask in range(1, len(s)):
+        d = (n>>mask) & 1
+        if d == 0 and d != ((n>>(mask-1)) & 1):
+            cnt += 1
 
-    print(ans)
+    match cnt:
+        case 0: print(0)
+        case 1: print(1)
+        case _: print(2)
