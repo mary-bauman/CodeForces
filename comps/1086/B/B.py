17179fc1b784442d8b822f21e484a848161cba14
Mary <marybauman503@gmail.com>
2026-03-14 10:30:36 -0400

    prepare for comp 1086

---
+++ comps/1086/B/B.py
@@ -0,0 +1,7 @@
+tests = int(input())
+for test in range(tests):
+    t = str(input())
+    good = True
+    
+    if good: print("YES")
+    else: print("NO")
---
+++ comps/1086/README.md
@@ -0,0 +1,4 @@
+Competition Round 1086
+Division 2
+Mar/14/2026 10:35UTC-4
+https://codeforces.com/contests/2208
