f6741bc15eb7f12560b53477151faefce95e057e
Mary <marybauman503@gmail.com>
2026-03-14 10:46:54 -0400

    A submitted

--- comps/1086/A/A.py
+++ comps/1086/A/A.py
@@ -1,7 +1,40 @@
+from collections import defaultdict
+
 tests = int(input())
 for test in range(tests):
-    t = str(input())
-    good = True
+    boardSize = int(input())
+    board = []
+    s = defaultdict(int)
+    for t in range(boardSize):
+        line = input().split()
+        board.append(line)
+        for c in line: s[c] += 1
+    exists = True
+    limit = boardSize
+
+    # 1 2
+    # 2 1
+
+    # 1 1 2
+    # 2 1 1
+    # 1 2 1
+
+    # 1 1 1 2
+    # 1 1 2 1
+    # 1 2 1 1
+    # 2 1 1 1
+
+    # if boardSize == 2: limit = 2
+    # elif boardSize == 3: limit = 6
+    # elif boardSize == 4: limit = 12
+
+
+    limit = (boardSize-1)* boardSize
+
+    for c in s:
+        if s[c] > limit:
+            exists = False
+            break
     
-    if good: print("YES")
+    if exists: print("YES")
     else: print("NO")
--- comps/1086/README.md
+++ comps/1086/README.md
@@ -1,4 +1,4 @@
 Competition Round 1086
 Division 2
-Mar/14/2026 10:35UTC-4
+Mar/14/2026 10:35 EST
 https://codeforces.com/contests/2208
