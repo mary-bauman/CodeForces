536081efe60a6ea41a29a8df43e02011da36b69c
Mary <marybauman503@gmail.com>
2026-03-14 15:59:20 -0400

    B accepted

--- comps/1086/B/B.py
+++ comps/1086/B/B.py
@@ -1,7 +1,28 @@
 tests = int(input())
 for test in range(tests):
-    t = str(input())
-    good = True
-    
-    if good: print("YES")
-    else: print("NO")
+    #n,k=numPlayableCards,p,m
+    numCards, k, winPosition, availEnergy = map(int, input().split())
+    cardCosts = list(map(int, input().split()))
+    ans = 0
+    while availEnergy >= min(cardCosts[:k]):
+        # print(f"cardCosts: {cardCosts}, availEnergy: {availEnergy}, winPosition: {winPosition}")
+        while k < winPosition:
+            i = cardCosts.index(min(cardCosts[:k]))
+            cost = cardCosts[i]
+            availEnergy -= cost
+            cardCosts.remove(cost)
+            cardCosts.append(cost)
+            winPosition -= 1
+        #now we can play winPisition
+        cost = cardCosts[winPosition-1]
+        if availEnergy < cost: availEnergy = -1
+        else:
+            cost = cardCosts[winPosition-1]
+            availEnergy -= cost
+            cardCosts.remove(cost)
+            cardCosts.append(cost)
+            winPosition = numCards
+            ans += 1
+    # print(f"Final cardCosts: {cardCosts}, availEnergy: {availEnergy}, winPosition: {winPosition}")
+    print(ans)
+    # print()
--- comps/1086/README.md
+++ comps/1086/README.md
@@ -2,3 +2,6 @@ Competition Round 1086
 Division 2
 Mar/14/2026 10:35 EST
 https://codeforces.com/contests/2208
+I did go to the gym at like 10:45 after solving A
+And missed the rest of the contest
+But I can solve after the contest :)
