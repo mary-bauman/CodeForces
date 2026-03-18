d45c9e47482533e707fda20ca44026e847ea9bd7
Mary <marybauman503@gmail.com>
2026-03-18 19:42:34 -0400

    C progress

--- comps/1086/C/C.py
+++ comps/1086/C/C.py
@@ -1,3 +1,14 @@
 tests = int(input())
 for test in range(tests):
+    numberOfTasks = int(input())
+    tasks = []
+    for _ in range(numberOfTasks):
+        #each task is c,p
+        #c is int, p is difficulty
+        tasks.append(list(map(int, input().split())))
+    maxPossiblePoints = 0.0
+    initialStamina = 1
     
+
+
+    print(maxPossiblePoints)
