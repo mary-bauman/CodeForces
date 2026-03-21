e9728b41781cd0f53baedccd811742e8e1ec6765
Mary <marybauman503@gmail.com>
2026-03-21 19:24:55 -0400

    TLE submission

--- comps/1086/C/C.py
+++ comps/1086/C/C.py
@@ -5,10 +5,29 @@ for test in range(tests):
     for _ in range(numberOfTasks):
         #each task is c,p
         #c is int, p is difficulty
-        tasks.append(list(map(int, input().split())))
-    maxPossiblePoints = 0.0
-    initialStamina = 1
-    
+        tasks.append(list(map(float, input().split())))
+    # print("tasks: ", tasks)
 
+    def makeChoice(task, stamina, points):
+        if task >= numberOfTasks: 
+            # print(f"at {points} points: no more tasks")
+            return points
+        
+        c, difficulty = tasks[task]
 
+        # print(f"c: {c}, difficulty: {difficulty}, stamina: {stamina}, points: {points}")
+
+        
+        # print("about to doNothing")
+        doNothing = float(makeChoice(task + 1, stamina, points))
+
+        #do task
+        points += stamina * c
+        stamina *= (1-(difficulty/100))
+        # print("about to do task")
+        doTask = float(makeChoice(task + 1, stamina, points))
+
+        return max(doNothing, doTask)
+
+    maxPossiblePoints = makeChoice(0, 1, 0)
     print(maxPossiblePoints)
