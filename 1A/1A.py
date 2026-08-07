55dd18bcca5dade4c24a6673571b31a407c9ddc1
Mary <marybauman503@gmail.com>
2026-08-06 20:02:46 -0400

    1A Accepted

--- 1A/1A.py
+++ 1A/1A.py
@@ -1,11 +1,12 @@
 import math
 
 def answer(n, m, a):
+    if a==1: return n*m
     if a>=n and a>=m: return 1
     if a>=n: #a<m
-        return math.ceil(a/m)
+        return math.ceil(m/a)
     if a>=m: #a<n
-        return math.ceil(a/n)
+        return math.ceil(n/a)
     # a<n and a<m
     return math.ceil(n/a)*math.ceil(m/a)
 
