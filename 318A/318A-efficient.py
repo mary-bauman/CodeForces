b459500cbda29819d799d225206a54162f1d4f65
Mary <marybauman503@gmail.com>
2026-08-13 14:32:21 -0400

    Accepted

--- 318A/318A-efficient.py
+++ 318A/318A-efficient.py
@@ -1,12 +1,4 @@
 n, k = map(int, input().split())
 
-if n % 2 == 0:
-    if k <= n // 2:
-        print((k * 2) - 1)
-    else:
-        print((k-(n//2))*2)
-else:
-    if k <= n // 2 + 1:
-        print((k * 2) - 1)
-    else:
-        print((k-(n//2 + 1))*2)
+if k <= (n+1) // 2: print((k * 2) - 1)
+else: print((k-((n+1)//2))*2)
